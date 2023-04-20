0224bb4a526c13c28b501412d85ac2154c8dfdfc
anitha <singamreddyanitha123@gmail.com>
2023-04-20 07:52:17 +0000

    myTasks

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import Home from './components/Home'
+
+import './App.css'
+
+// These are the lists used in the application. You can move them to any component needed.
+/*
+const tagsList = [
+  {
+    optionId: 'HEALTH',
+    displayText: 'Health',
+  },
+  {
+    optionId: 'EDUCATION',
+    displayText: 'Education',
+  },
+  {
+    optionId: 'ENTERTAINMENT',
+    displayText: 'Entertainment',
+  },
+  {
+    optionId: 'SPORTS',
+    displayText: 'Sports',
+  },
+  {
+    optionId: 'TRAVEL',
+    displayText: 'Travel',
+  },
+  {
+    optionId: 'OTHERS',
+    displayText: 'Others',
+  },
+]
+
+*/
+
+// Replace your code here
+const App = () => <Home />
+
+export default App
